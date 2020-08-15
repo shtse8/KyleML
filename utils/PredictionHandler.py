73a7eb5b6b088d5f474337bffe937c409e85b368
shtse8 <shtse8@outlook.com>
2020-08-15 18:50:13 +0800

    added a prediction handler

---
+++ utils/PredictionHandler.py
@@ -0,0 +1,33 @@
+import numpy as np
+import math
+
+class PredictionHandler:
+    def __init__(self, prediction, mask = None):
+        self.prediction = prediction
+        if mask is not None:
+            self.setMask(mask) 
+    
+    def setMask(self, mask):
+        if not mask.any():
+            raise Exception("No actions")
+        self.mask = mask
+        return self
+        
+    def applyMask(self, fillValue = 0):
+        if self.mask is None:
+            return self.prediction
+        return np.array([self.prediction[i] if self.mask[i] else fillValue for i in range(len(self.prediction))])
+        
+    def getBestAction(self):
+        prediction = self.applyMask(-math.inf)
+        return prediction.argmax()
+        
+    def getRandomAction(self):
+        prediction = self.applyMask(0)
+        predictionSum = prediction.sum()
+        if predictionSum != 0:
+            prediction = prediction / predictionSum
+        else:
+            prediction = mask / mask.sum()
+        return np.random.choice(len(prediction), p=prediction)
+    

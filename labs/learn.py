ed8a9dfed0a3d652066924bc3dea43ff2f4fd2aa
shtse8 <shtse8@outlook.com>
2020-08-15 23:12:53 +0800

    update learn

--- labs/learn.py
+++ labs/learn.py
@@ -1,5 +1,7 @@
-import os
-from pathlib import Path
-path = os.path.join(os.path.dirname(__file__), "weights", "a2c", "model.h5")
-# Path(path).mkdir(parents=True, exist_ok=True)
-print(os.path.dirname(path), __file__)
+import numpy as np
+
+
+a = np.array([[1, 2, 3], [4, 5, 6]])
+i = np.array([[1], [0]])
+# print(a[i])
+print(np.take_along_axis(a, i, 1))

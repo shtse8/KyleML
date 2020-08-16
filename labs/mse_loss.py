6d8a941712d2a52668b1025abb55b2a02403d4b2
shtse8 <shtse8@outlook.com>
2020-08-17 02:30:21 +0800

    added mse loss lab note

---
+++ labs/mse_loss.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+s = torch.FloatTensor(np.array([0.1, 0.2, 0.4, 0.3]))
+
+
+print(s.unsqueeze(1).squeeze(1))
+
+t = torch.FloatTensor(np.array([1, 2, 3, 4]))
+print(s, t)
+loss = nn.MSELoss()(s.unsqueeze(1), t.unsqueeze(1))
+print(loss)
+loss = nn.MSELoss()(s, t.unsqueeze(1))
+print(loss)
+loss = nn.MSELoss()(s.unsqueeze(1), t)
+print(loss)
+loss = nn.MSELoss()(s, t)
+print(loss)
+
+loss = (t.unsqueeze(1) - s.unsqueeze(1)).pow(2).mean()
+print(loss)
+loss = (t.unsqueeze(1) - s).pow(2).mean()
+print(loss)
+loss = (t - s.unsqueeze(1)).pow(2).mean()
+print(loss)
+loss = (t - s).pow(2).mean()
+print(loss)

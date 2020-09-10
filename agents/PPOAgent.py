33c2f8a141a36adc1f3eea0bda14730d9f928e1a
shtse8 <shtse8@outlook.com>
2020-09-11 03:41:21 +0800

    We need to normalize the reward to prevent the GRU becomes all 1 and -1.

--- agents/PPOAgent.py
+++ agents/PPOAgent.py
@@ -319,10 +319,10 @@ class GRULayers(nn.Module):
     def forward(self, x, h):
         # x: (B, N) -> (1, B, N)
         # h: (B, L, H) -> (L, B, H)
-        x, h = self.gru(x.unsqueeze(0), h.transpose(0, 1).contiguous())
+        x, h = self.gru(x.unsqueeze(0), h.transpose(0, 1))
         # x: (1, B, N) -> (B, N)
         # h: (L, B, H) -> (B, L, H)
-        return x.squeeze(0), h.transpose(0, 1).contiguous()
+        return x.squeeze(0), h.transpose(0, 1)
 
 
 class PPONetwork(Network):
@@ -353,7 +353,7 @@ class PPONetwork(Network):
 
     def _body(self, x, h):
         x = self.body(x)
-        # x, h = self.gru(x, h)
+        x, h = self.gru(x, h)
         return x, h
 
     def _policy(self, x, m=None):
@@ -560,8 +560,9 @@ class PPOAlgo(Algo[PPOConfig]):
         # Normalize advantages
         # https://github.com/openai/baselines/blob/master/baselines/ppo2/model.py#L139
         # advantages = memory.select(lambda x: x.reward) - memory.select(lambda x: x.value)
-        advantages = memory.select(lambda x: x.advantage)
-        advantages = Function.normalize(advantages)
+        # We need to normalize the reward to prevent the GRU becomes all 1 and -1.
+        returns = memory.select(lambda x: x.reward)
+        returns = Function.normalize(returns)
 
         for i in range(n_miniBatch):
             startIndex = i * batchSize
@@ -576,14 +577,14 @@ class PPOAlgo(Algo[PPOConfig]):
                 torch.bool, self.device)
             old_log_probs = minibatch.select(
                 lambda x: x.action.log).toTensor(torch.float, self.device)
-            returns = minibatch.select(lambda x: x.reward).toTensor(
+            batch_returns = returns.get(
+                startIndex, batchSize).toTensor(
                 torch.float, self.device)
-            old_values = minibatch.select(
+            batch_values = minibatch.select(
                 lambda x: x.value).toTensor(torch.float, self.device)
             hiddenStates = minibatch.select(
                 lambda x: x.hiddenState).toTensor(torch.float, self.device)
-            batch_advantages = advantages.get(
-                startIndex, batchSize).toTensor(torch.float, self.device)
+            batch_advantages = batch_returns - batch_values
 
             probs, values, _ = network(states, hiddenStates, masks)
             values = values.squeeze(-1)
@@ -607,11 +608,11 @@ class PPOAlgo(Algo[PPOConfig]):
             # Clip the value to reduce variability during Critic training
             # https://github.com/openai/baselines/blob/master/baselines/ppo2/model.py#L66-L75
             # https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail/blob/master/a2c_ppo_acktr/algo/ppo.py#L69-L75
-            value_loss1 = (returns - values).pow(2)
-            valuesClipped = old_values + \
-                torch.clamp(values - old_values, -
+            value_loss1 = (batch_returns - values).pow(2)
+            valuesClipped = batch_values + \
+                torch.clamp(values - batch_values, -
                             self.config.epsClip, self.config.epsClip)
-            value_loss2 = (returns - valuesClipped).pow(2)
+            value_loss2 = (batch_returns - valuesClipped).pow(2)
             value_loss = 0.5 * torch.max(value_loss1, value_loss2).mean()
 
             # MSE Loss

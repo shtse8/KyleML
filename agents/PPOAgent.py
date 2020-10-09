bbbff0a7d4fb42a6e3d39f24a7f797fe2c40f269
shtse8 <shtse8@outlook.com>
2020-10-09 19:52:23 +0800

    study GAE added more layers for stableness

--- agents/PPOAgent.py
+++ agents/PPOAgent.py
@@ -30,7 +30,7 @@ from .Agent import Base, EvaluatorService, SyncContext, Action, Config, Role, Al
 from utils.PipedProcess import Process, PipedProcess
 from utils.Normalizer import RangeNormalizer, StdNormalizer
 from utils.Message import NetworkInfo, LearnReport, EnvReport
-from utils.Network import Network, BodyLayers, GRULayers
+from utils.Network import Network, BodyLayers, GRULayers, FCLayers
 from utils.multiprocessing import Proxy
 from utils.PredictionHandler import PredictionHandler
 from utils.KyleList import KyleList
@@ -52,14 +52,12 @@ class ICMNetwork(Network):
             nn.Linear(hidden_nodes, hidden_nodes))
         
         self.inverseNet = nn.Sequential(
-            nn.Linear(hidden_nodes * 2, hidden_nodes),
-            nn.ELU(),
+            FCLayers(hidden_nodes * 2, hidden_nodes, 3, nn.ELU),
             nn.Linear(hidden_nodes, output_size)
         )
         
         self.forwardNet = nn.Sequential(
-            nn.Linear(output_size + hidden_nodes, hidden_nodes),
-            nn.ELU(),
+            FCLayers(output_size + hidden_nodes, hidden_nodes, 3, nn.ELU),
             nn.Linear(hidden_nodes, hidden_nodes),
         )
 
@@ -82,22 +80,22 @@ class PPONetwork(Network):
     def __init__(self, inputShape, n_outputs):
         super().__init__(inputShape, n_outputs)
 
-        self.eps = torch.finfo(torch.float).eps
+        # self.eps = torch.finfo(torch.float).eps
         hidden_nodes = 256
         # semi_hidden_nodes = hidden_nodes // 2
-        # self.body = BodyLayers(inputShape, hidden_nodes)
+        self.body = BodyLayers(inputShape, hidden_nodes)
         
-        self.gru = GRULayers(hidden_nodes, hidden_nodes, 
+        self.gru = GRULayers(self.body.num_output, hidden_nodes, 
             num_layers=2, bidirectional=True)
 
         # Define policy head
         self.policy = nn.Sequential(
-            BodyLayers(inputShape, hidden_nodes),
+            FCLayers(self.gru.num_output, hidden_nodes, 3, nn.ELU),
             nn.Linear(hidden_nodes, n_outputs))
 
         # Define value head
         self.value = nn.Sequential(
-            BodyLayers(inputShape, hidden_nodes),
+            FCLayers(self.gru.num_output, hidden_nodes, 3, nn.ELU),
             nn.Linear(hidden_nodes, 1))
 
         self.initWeights()
@@ -111,8 +109,8 @@ class PPONetwork(Network):
         return self.gru.getInitHiddenState(device)
 
     def _body(self, x, h):
-        # x = self.body(x)
-        # x, h = self.gru(x, h)
+        x = self.body(x)
+        x, h = self.gru(x, h)
         return x, h
 
     def _policy(self, x, m=None):
@@ -123,15 +121,15 @@ class PPONetwork(Network):
         return x
 
     def forward(self, x, h, m=None):
-        # x, h = self._body(x, h)
+        x, h = self._body(x, h)
         return self._policy(x, m), self.value(x), h
 
     def getPolicy(self, x, h, m=None):
-        # x, h = self._body(x, h)
+        x, h = self._body(x, h)
         return self._policy(x, m), h
 
     def getValue(self, x, h):
-        # x, h = self._body(x, h)
+        x, h = self._body(x, h)
         return self.value(x), h
 
 
@@ -174,7 +172,20 @@ class AgentHandler:
         self.hiddenState = hiddenState
         probs = probs.cpu().detach().numpy()
         value = value.item()
+
+        # valid_acts, valid_probs = [], []尸尸
         if isTraining:
+            # info = self.env.getInfo()
+            # for i, p in enumerate(probs):
+            #     if info.mask[i]:
+            #         valid_acts.append(i)
+            #         valid_probs.append(p)
+            # valid_acts = np.array(valid_acts)
+            # valid_probs = np.array(valid_probs)
+            # noise_probs = 0.75 * valid_probs + 0.25 * np.random.dirichlet(0.3 * np.ones(len(valid_probs)))
+            # noise_probs = noise_probs / noise_probs.sum()
+            # index = np.random.choice(len(noise_probs), p=noise_probs)
+            # index = valid_acts[index]
             index = np.random.choice(len(probs), p=probs)
         else:
             index = np.argmax(probs)
@@ -183,6 +194,8 @@ class AgentHandler:
             probs=probs,
             value=value)
 
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
 
 
 class PPOHandler(AlgoHandler):
@@ -192,7 +205,7 @@ class PPOHandler(AlgoHandler):
         self.network.to(self.device)
         self.icm = ICMNetwork(env.observationShape, env.actionSpace)
         self.icm.to(self.device)
-        self.rewardNormalizer = StdNormalizer()
+        self.rewardNormalizer = RangeNormalizer()
         if role == Role.Trainer:
             self.network.buildOptimizer(self.config.learningRate)
             self.icm.buildOptimizer(self.config.learningRate)
@@ -234,30 +247,35 @@ class PPOHandler(AlgoHandler):
             # GAE (General Advantage Estimation)
             # Paper: https://arxiv.org/abs/1506.02438
             # Code: https://github.com/openai/baselines/blob/master/baselines/ppo2/runner.py#L55-L64
+            # gaeCoeff = 0, advantage = reward + next_value - value
+            # gaeCoeff = 1, advantage = total_reward - value
+            intrinsic_weight = 0.01 / max(1, np.mean(intrinsic_rewards))
+            # print(intrinsic_weight)
             lastAdvantage = 0
             for i, transition in reversed(list(enumerate(memory))):
-                transition.reward = extrinsic_rewards[i] + intrinsic_rewards[i] + self.config.gamma * lastValue * (1 - transition.next.done)
-                detlas = transition.reward - transition.action.value
-                # from baseline
-                # https://github.com/openai/baselines/blob/master/baselines/ppo2/runner.py#L65
-                transition.advantage = detlas + self.config.gamma * self.config.gaeCoeff * lastAdvantage * (1 - transition.next.done)
+                transition.reward = extrinsic_rewards[i]
+                transition.reward += intrinsic_weight * intrinsic_rewards[i]
+                # print(extrinsic_rewards[i], intrinsic_rewards[i])
+                transition.advantage = transition.reward + self.config.gamma * lastValue * (1 - transition.next.done) - transition.action.value
+                transition.advantage += self.config.gamma * self.config.gaeCoeff * lastAdvantage * (1 - transition.next.done)
+                transition.reward = transition.advantage + transition.action.value
                 lastAdvantage = transition.advantage
-                lastValue = transition.reward
+                lastValue = transition.action.value
 
     def learn(self, memory):
         self.network.train()
         self.icm.train()
         
         batchSize = min(memory.size(), self.config.batchSize)
-        n_miniBatch = memory.size() // batchSize
+        n_miniBatch = math.ceil(memory.size() / batchSize)
 
         # returns = memory.select(lambda x: x.reward).toArray()
         # returns = KyleList(self.rewardNormalizer.normalize(returns, update=True))
         # Normalize advantages
         # https://github.com/openai/baselines/blob/master/baselines/ppo2/model.py#L139
-        # advantages = memory.select(lambda x: x.reward) - memory.select(lambda x: x.action.value)
         # advantages = Function.normalize(advantages)
         # advantages = returns - memory.select(lambda x: x.action.value)
+        # advantages = memory.select(lambda x: x.advantage)
         # advantages = Function.normalize(advantages)
 
         losses = []
@@ -278,6 +296,7 @@ class PPOHandler(AlgoHandler):
                 batch_nextStates = minibatch.select(lambda x: x.next.state).toTensor(torch.float, self.device)
                 batch_actions = minibatch.select(lambda x: x.action.index).toTensor(torch.long, self.device)
                 batch_log_probs = minibatch.select(lambda x: x.action.log).toTensor(torch.float, self.device)
+                # batch_probs = minibatch.select(lambda x: x.action.probs).toTensor(torch.float, self.device)
                 batch_returns = minibatch.select(lambda x: x.reward).toTensor(torch.float, self.device)
                 # batch_returns = returns.get(startIndex, batchSize).toTensor(torch.float, self.device)
                 # batch_values = minibatch.select(lambda x: x.action.value).toTensor(torch.float, self.device)
@@ -288,7 +307,11 @@ class PPOHandler(AlgoHandler):
                 
                 inverse_loss = nn.CrossEntropyLoss()(pred_action, batch_actions)
                 forward_loss = nn.MSELoss()(pred_next_state_feature, real_next_state_feature.detach())
-                icm_loss = inverse_loss + forward_loss
+                if batch_actions[0] == 2:
+                    print(batch_actions[0], nn.MSELoss(reduction="none")(pred_next_state_feature, real_next_state_feature.detach())[0].mean())
+                icm_loss = forward_loss
+                # icm_loss = inverse_loss + forward_loss
+                # icm_loss = 0
                 
                 # for AC Network
                 batch_advantages = minibatch.select(lambda x: x.advantage).toTensor(torch.float, self.device)
@@ -307,16 +330,22 @@ class PPOHandler(AlgoHandler):
                 # Maximize Policy Loss (Rewards)
                 policy_loss = -torch.min(policy_losses1, policy_losses2).mean()
 
+                # Maximize Entropy Loss  (MSE)
+                # print(memory[0].action.probs)
+                # print(batch_probs[0], dist.entropy()[0])
+                entropy_loss = -dist.entropy().mean()
+
                 # Minimize Value Loss  (MSE)
-                value_loss = (batch_returns - values).pow(2).mean()
+                value_loss = nn.MSELoss()(values, batch_returns)
+                # value_loss = (batch_returns - values).pow(2).mean()
             
-                network_loss = policy_loss + value_loss
+                network_loss = policy_loss + 0.01 * entropy_loss + value_loss
 
                 # Calculating Total loss
                 # the weight of this minibatch
                 weight = len(minibatch) / len(memory)
                 loss = (network_loss + icm_loss) * weight
-                # print("Loss:", loss, policy_loss, entropy_loss, value_loss, weight)
+                # print("Loss:", loss, policy_loss, entropy_loss, value_loss, inverse_loss, forward_loss)
 
                 # Accumulating the loss to the graph
                 loss.backward()
@@ -325,7 +354,8 @@ class PPOHandler(AlgoHandler):
             # Chip grad with norm
             # https://github.com/openai/baselines/blob/9b68103b737ac46bc201dfb3121cfa5df2127e53/baselines/ppo2/model.py#L107
             # nn.utils.clip_grad.clip_grad_norm_(self.network.parameters(), 0.5)
-            # nn.utils.clip_grad.clip_grad_norm_(self.icm.parameters(), 0.5)
+            nn.utils.clip_grad.clip_grad_norm_(self.network.parameters(), 10)
+            # nn.utils.clip_grad.clip_grad_norm_(self.icm.parameters(), 10)
 
             self.network.optimizer.step()
             self.icm.optimizer.step()

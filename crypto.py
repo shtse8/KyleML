435354154a1694b7680eb26054315f5a62a331d7
Kyle Tse <shtse8@outlook.com>
2022-06-02 15:18:41 +0800

    it's stable to train to 99.5% acc. but need further optimization.

--- crypto.py
+++ crypto.py
@@ -151,31 +151,86 @@ class CryptoDataLoaderIterator:
             data = tuple([sample.pin_memory(self.loader.pin_memory_device) for sample in data])
         return data
 
-
-def binary(num, bits):
-    return ((num & (1 << np.arange(bits))[::-1]) > 0).astype(int)
-
-
-def batches(input_list, batch_size):
-    # try:
-    idx = 0
-    while idx < len(input_list):
-        yield input_list[idx: min(idx + batch_size, len(input_list))]
-        idx += batch_size
-    # except:
-    #     result = []
-    #     iterator = iter(input_list)
-    #     while (x := next(iterator, None)) is not None:
-    #         result.append(x)
-    #         if len(result) >= batch_size:
-    #             yield result
-    #             result = []
-    #     yield result
+#
+# def binary(num, bits):
+#     return ((num & (1 << np.arange(bits))[::-1]) > 0).astype(int)
+#
+#
+# def batches(input_list, batch_size):
+#     # try:
+#     idx = 0
+#     while idx < len(input_list):
+#         yield input_list[idx: min(idx + batch_size, len(input_list))]
+#         idx += batch_size
+#     # except:
+#     #     result = []
+#     #     iterator = iter(input_list)
+#     #     while (x := next(iterator, None)) is not None:
+#     #         result.append(x)
+#     #         if len(result) >= batch_size:
+#     #             yield result
+#     #             result = []
+#     #     yield result
 
 
 # def binary(x, bits):
 #     mask = 2**torch.arange(bits).to(x.device, x.dtype)
 #     return x.unsqueeze(-1).bitwise_and(mask).ne(0).byte()
+
+class SwitchNorm1d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.997, using_moving_average=True):
+        super(SwitchNorm1d, self).__init__()
+        self.eps = eps
+        self.momentum = momentum
+        self.using_moving_average = using_moving_average
+        self.weight = nn.Parameter(torch.ones(1, num_features))
+        self.bias = nn.Parameter(torch.zeros(1, num_features))
+        self.mean_weight = nn.Parameter(torch.ones(2))
+        self.var_weight = nn.Parameter(torch.ones(2))
+        self.register_buffer('running_mean', torch.zeros(1, num_features))
+        self.register_buffer('running_var', torch.zeros(1, num_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.running_mean.zero_()
+        self.running_var.zero_()
+        self.weight.data.fill_(1)
+        self.bias.data.zero_()
+
+    def _check_input_dim(self, input):
+        if input.dim() != 2:
+            raise ValueError('expected 2D input (got {}D input)'
+                             .format(input.dim()))
+
+    def forward(self, x):
+        self._check_input_dim(x)
+        mean_ln = x.mean(1, keepdim=True)
+        var_ln = x.var(1, keepdim=True)
+
+        if self.training:
+            mean_bn = x.mean(0, keepdim=True)
+            var_bn = x.var(0, keepdim=True)
+            if self.using_moving_average:
+                self.running_mean.mul_(self.momentum)
+                self.running_mean.add_((1 - self.momentum) * mean_bn.data)
+                self.running_var.mul_(self.momentum)
+                self.running_var.add_((1 - self.momentum) * var_bn.data)
+            else:
+                self.running_mean.add_(mean_bn.data)
+                self.running_var.add_(mean_bn.data ** 2 + var_bn.data)
+        else:
+            mean_bn = torch.autograd.Variable(self.running_mean)
+            var_bn = torch.autograd.Variable(self.running_var)
+
+        softmax = nn.Softmax(0)
+        mean_weight = softmax(self.mean_weight)
+        var_weight = softmax(self.var_weight)
+
+        mean = mean_weight[0] * mean_ln + mean_weight[1] * mean_bn
+        var = var_weight[0] * var_ln + var_weight[1] * var_bn
+
+        x = (x - mean) / (var + self.eps).sqrt()
+        return x * self.weight + self.bias
 
 
 def signal_handler(sig, frame):
@@ -190,13 +245,18 @@ class Network(nn.Module):
 
     def __init__(self, in_nodes: int, out_nodes: int):
         super(Network, self).__init__()
-        hidden_nodes = 128
+        hidden_nodes = 256
         self.layers = nn.Sequential(
-            # nn.BatchNorm1d(in_nodes),
-            nn.Linear(in_nodes, hidden_nodes),
+            nn.Linear(in_nodes, 256),
             nn.ELU(),
-            nn.BatchNorm1d(hidden_nodes),
-            nn.Linear(hidden_nodes, out_nodes)
+            SwitchNorm1d(256),
+            # nn.Linear(256, 128),
+            # nn.ELU(),
+            # SwitchNorm1d(128),
+            # nn.Linear(128, 64),
+            # nn.ELU(),
+            # SwitchNorm1d(64),
+            nn.Linear(256, out_nodes)
         )
 
     def forward(self, x):
@@ -352,7 +412,7 @@ async def main():
     network = Network(in_features, 2).to(device)
     epoch = 0
 
-    optimizer = optim.AdamW(network.parameters(), lr=1e-5)
+    optimizer = optim.AdamW(network.parameters(), lr=1e-6)
     # optimizer = optim.SGD(network.parameters(), lr=1e-4, momentum=0.9)
     # schedular = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
     schedular = optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True)
